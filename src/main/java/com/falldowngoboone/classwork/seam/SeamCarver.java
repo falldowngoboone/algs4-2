dcc2c0e1e0df86b1550e29e5d57173e264b4a7d3
Ryan Boone <falldowngoboone@gmail.com>
2017-12-09 10:47:58 -0600

    Almost there

--- src/main/java/com/falldowngoboone/classwork/seam/SeamCarver.java
+++ src/main/java/com/falldowngoboone/classwork/seam/SeamCarver.java
@@ -1,116 +1,211 @@
 /******************************************************************************
- *  Compilation:  javac SeamCarver.java
- * 
- *  Content aware image resizing
- ******************************************************************************/
+*  Compilation:  javac SeamCarver.java
+* 
+*  Content aware image resizing
+******************************************************************************/
 package com.falldowngoboone.classwork.seam;
 
 import edu.princeton.cs.algs4.Picture;
 import java.awt.Color;
-
-/* 
- * Corner cases.
- * =============
- * 
- * Your code should throw a `java.lang.IllegalArgumentException` when a 
- * constructor or method is called with an invalid argument, as documented 
- * below:
- * 
- * - By convention, the indices x and y are integers between 0 and width − 1 and 
- *   between 0 and height − 1 respectively, where width is the width of the 
- *   current image and height is the height. Throw a java.lang.
- *   IllegalArgumentException if either x or y is outside its prescribed range.
- * - Throw a java.lang.IllegalArgumentException if the constructor, 
- *   removeVerticalSeam(), or removeHorizontalSeam() is called with a null 
- *   argument.
- * - Throw a java.lang.IllegalArgumentException if removeVerticalSeam() or 
- *   removeHorizontalSeam() is called with an array of the wrong length or if the 
- *   array is not a valid seam (i.e., either an entry is outside its prescribed 
- *   range or two adjacent entries differ by more than 1).
- * - Throw a java.lang.IllegalArgumentException if removeVerticalSeam() is called 
- *   when the width of the picture is less than or equal to 1 or if 
- *   removeHorizontalSeam() is called when the height of the picture is less than 
- *   or equal to 1.
- * 
- * Constructor. 
- * ============
- * 
- * The data type may not mutate the Picture argument to the constructor.
- */
+import static java.lang.System.arraycopy;
 
 public class SeamCarver {
-    private Picture picture;
+    private int W;
+    private int H;
+    Pixel[][] pixels;
+    private boolean isTransposed = false;
+
+    private class Pixel {
+        private Color color;
+        private double energy;
+
+        public Pixel(Color color) {
+            this.color = color;
+        }
+    }
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
-        this.picture = new Picture(picture);
+        if (null == picture)
+            throw new IllegalArgumentException("SeamCarver must be called with a valid Picture instance.");
+        W = picture.width();
+        H = picture.height();
+        pixels = new Pixel[H][W];
+        for (int y = 0; y < H; y++)
+            for (int x = 0; x < W; x++) {
+                pixels[y][x] = new Pixel(picture.get(x, y));
+                pixels[y][x].energy = energy(x, y, picture);
+            }
+    }
+
+    private Pixel getPixel(int x, int y) {
+        return isValidPixel(x, y) ? pixels[y][x] : null;
     }
 
     // current picture
     public Picture picture() {
+        if (isTransposed)
+            transpose();
+        Picture picture = new Picture(W, H);
+        for (int x = 0; x < W; x++)
+            for (int y = 0; y < H; y++)
+                picture.set(x, y, getPixel(x, y).color);
         return picture;
     }
 
     // width of current picture
     public int width() {
-        return picture.width();
+        return isTransposed ? H : W;
     }
 
     // height of current picture
     public int height() {
-        return picture.height();
+        return isTransposed ? W : H;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        validatePixelAndMaybeThrowError(x, y);
+        if (isTransposed) {
+            transpose();
+        }
+        if (!isValidPixel(x, y))
+            throw new IllegalArgumentException();
 
         if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1)
             return 1000;
 
-        Color up    = picture.get(x, y-1), 
-              down  = picture.get(x, y+1), 
-              left  = picture.get(x-1, y), 
-              right = picture.get(x+1, y);
+        Color up = getPixel(x, y - 1).color, down = getPixel(x, y + 1).color, left = getPixel(x - 1, y).color,
+                right = getPixel(x + 1, y).color;
+
+        return Math.sqrt(squareDelta(up, down) + squareDelta(left, right));
+    }
+
+    private double energy(int x, int y, Picture picture) {
+        if (!isValidPixel(x, y))
+            throw new IllegalArgumentException();
+
+        if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1)
+            return 1000;
+
+        Color up = picture.get(x, y - 1), down = picture.get(x, y + 1), left = picture.get(x - 1, y),
+                right = picture.get(x + 1, y);
 
         return Math.sqrt(squareDelta(up, down) + squareDelta(left, right));
     }
 
     private double squareDelta(Color a, Color b) {
-        double deltaR = a.getRed() - b.getRed(), 
-               deltaG = a.getGreen() - b.getGreen(), 
-               deltaB = a.getBlue() - b.getBlue();
+        double deltaR = a.getRed() - b.getRed(), deltaG = a.getGreen() - b.getGreen(),
+                deltaB = a.getBlue() - b.getBlue();
 
         return Math.pow(deltaR, 2) + Math.pow(deltaG, 2) + Math.pow(deltaB, 2);
     }
 
     // sequence of indices for horizontal seam
-    // public int[] findHorizontalSeam() {}
+    public int[] findHorizontalSeam() {
+        if (!isTransposed)
+            transpose();
+        return findSeam();
+    }
 
     // sequence of indices for vertical seam
-    // public int[] findVerticalSeam() {}
+    public int[] findVerticalSeam() {
+        if (isTransposed)
+            transpose();
+        return findSeam();
+    }
+
+    private int[] findSeam() {
+        int[] verticalSeam = new int[H];
+        double[][] distTo = new double[H][W]; // push accumulated energy to this
+        int[][] pixelTo = new int[H][W]; // push x to this
+        for (int y = 0; y < H; y++)
+            for (int x = 0; x < W; x++) {
+                if (y == 0)
+                    distTo[y][x] = 0;
+                else
+                    distTo[y][x] = Double.POSITIVE_INFINITY;
+            }
+        for (int y = 0; y < H - 1; y++)
+            for (int x = 0; x < W; x++) {
+                Pixel p1 = getPixel(x, y);
+                int y2 = y + 1;
+                for (int x2 = x - 1; x2 <= x + 1; x2++) {
+                    Pixel p2 = getPixel(x2, y2);
+                    if (p2 == null)
+                        continue;
+                    if (distTo[y][x] + p2.energy < distTo[y2][x2]) {
+                        distTo[y2][x2] = distTo[y][x] + p2.energy;
+                        pixelTo[y2][x2] = x;
+                    }
+                }
+            }
+        int x = 0;
+        for (int i = 0; i < W; i++) {
+            if (distTo[H - 1][i] < distTo[H - 1][x])
+                x = i;
+        }
+        for (int y = H - 1; y >= 0; y--) {
+            verticalSeam[y] = x;
+            x = pixelTo[y][x];
+        }
+        return verticalSeam;
+    }
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-        // create a new picture of height 1px less
-        // iterate through the current picture
-        // if the current pixel is not in the seam
-            // copy the pixel to the new picture
-        // set the picture to the new picture
+        if (1 == H)
+            throw new IllegalArgumentException("Cannot remove seam; width is 1px.");
+        // transpose the picture and apply removeVerticalSeam()
+        // so there needs to be a boolean for storing picture state
+        // as well as a transpose method that transposes the picture based on that boolean
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        // create a new picture of width 1px less
-        // iterate through the current picture
-        // if the current pixel is not in the seam
-            // copy the pixel to the new picture
-        // set the picture to the new picture
+        if (1 == W)
+            throw new IllegalArgumentException("Cannot remove seam; width is 1px.");
+        validateSeamAndMaybeThrowError(seam, H);
+        for (int i = 0; i < H; i++) {
+            if (!isValidPixel(seam[i], i))
+                throw new IllegalArgumentException();
+            // remove pixel and shift via arraycopy
+        }
     }
 
-    private void validatePixelAndMaybeThrowError(int x, int y) {
-        if (x < 0 || y < 0 || x >= width() || y >= height()) {
-            throw new IllegalArgumentException();            
+    private boolean isValidPixel(int x, int y) {
+        return x >= 0 && y >= 0 && x < width() && y < height();
+    }
+
+    private void validateSeamAndMaybeThrowError(int[] seam, int expectedLength) {
+        if (null == seam)
+            throw new IllegalArgumentException("Method must be supplied a valid int[] seam instance.");
+        if (seam.length != expectedLength)
+            throw new IllegalArgumentException(
+                    "Expected seam length of " + expectedLength + ", recevied: " + seam.length);
+        int prev = -1;
+        for (int current : seam) {
+            if (prev != -1 && prev - current < -1 || prev - current > 1)
+                throw new IllegalArgumentException("Seam is unconnected.");
+            prev = current;
         }
+    }
+
+    private void transpose() {
+        Pixel[][] newPixels = new Pixel[W][H];
+        if (!isTransposed) {
+            for (int y = 0; y < H; y++)
+                for (int x = 0; x < W; x++)
+                    newPixels[W - 1 - x][y] = getPixel(x, y);
+        } else {
+            // TODO: fix me!!!
+            for (int x = 0; x < W; x++)
+                for (int y = 0; y < H; y++)
+                    newPixels[x][H - 1 - y] = getPixel(x, y); // pixels[yH][xW]
+        }
+        pixels = newPixels;
+        int T = W;
+        W = H;
+        H = T;
+        isTransposed = !isTransposed;
     }
 }
--- src/test/java/com/falldowngoboone/classwork/seam/SeamCarverTest.java
+++ src/test/java/com/falldowngoboone/classwork/seam/SeamCarverTest.java
@@ -4,29 +4,130 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import com.falldowngoboone.classwork.TestFolder;
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarverTest {
-    SeamCarver sc1;
+    SeamCarver sc3x4;
+    SeamCarver sc1x1;
+    SeamCarver sc4x6;
+    SeamCarver sc7x10;
     TestFolder testFolder = new TestFolder("seam");
-    Picture testPic1 = new Picture(testFolder.getFilePath("3x4.png"));
+    Picture testPic3x4 = new Picture(testFolder.getFilePath("3x4.png"));
+    Picture testPic1x1 = new Picture(testFolder.getFilePath("1x1.png"));
+    Picture testPic4x6 = new Picture(testFolder.getFilePath("4x6.png"));
+    Picture testPic7x10 = new Picture(testFolder.getFilePath("7x10.png"));
 
     @BeforeEach
     public void set_up() {
-        sc1 = new SeamCarver(testPic1);
+        sc3x4 = new SeamCarver(testPic3x4);
+        sc1x1 = new SeamCarver(testPic1x1);
+        sc4x6 = new SeamCarver(testPic4x6);
+        sc7x10 = new SeamCarver(testPic7x10);
+    }
+
+    @Test
+    public void picture_withoutProcessing_ReturnsOriginalPicture() {
+        assertEquals(sc3x4.picture(), testPic3x4);
     }
 
     @Test
     public void test_energy_correctness() {
         // refer to [picName].printseams.txt for confirmation on [picName].png
-        assertEquals(sc1.energy(0,0), 1000.0);
-        assertEquals(sc1.energy(1,1), 228.52789764052878);
+        assertEquals(1000.0, sc3x4.energy(0, 0));
+        assertEquals(228.52789764052878, sc3x4.energy(1, 1));
     }
 
     @Test
     public void test_energy_invalid_pixel() {
-        // should throw IllegalArgumentException
-        assertThrows(IllegalArgumentException.class, () -> sc1.energy(4,5));
+        assertThrows(IllegalArgumentException.class, () -> sc3x4.energy(4, 5));
+    }
+
+    @Test
+    public void picture_afterRemoveHorizontalSeam_DoesNotThrowException() {
+        sc3x4.removeHorizontalSeam(new int[] { 1, 1, 1 });
+        sc3x4.picture();
+    }
+
+    @Test
+    public void removeHorizontalSeam_ValidSeam_PictureIsOnePixelShorter() {
+        // sc3x4.removeHorizontalSeam(new int[] {1,1,1});
+        // Picture pic = sc3x4.picture();
+        // assertEquals(3, sc3x4.height());
+        // assertEquals(3, pic.height());
+    }
+
+    @Test
+    public void removeVerticalSeam_ValidSeam_PictureIsOnePixelNarrower() {
+        // sc3x4.removeVerticalSeam(new int[] {1,1,1,1});
+        // Picture pic = sc3x4.picture();
+        // assertEquals(2, sc3x4.width());
+        // assertEquals(2, pic.width());
+    }
+
+    @Test
+    public void removeHorizontalSeam_ValidSeame_DoesNotThrowException() {
+        int[] valid = new int[] { 1, 1, 1 };
+        sc3x4.removeHorizontalSeam(valid);
+    }
+
+    @Test
+    public void removeVerticalSeam_ValidSeame_DoesNotThrowException() {
+        int[] valid = new int[] { 1, 1, 1, 1 };
+        sc3x4.removeVerticalSeam(valid);
+    }
+
+    @Test
+    public void removeVerticalSeam_SeamWrongLength_ThrowsIllegalArgumentException() {
+        int[] tooLong = new int[sc3x4.height() + 1];
+        assertThrows(IllegalArgumentException.class, () -> sc3x4.removeVerticalSeam(tooLong));
+    }
+
+    @Test
+    public void removeVerticalSeam_UnconnectedSeam_ThrowsIllegalArgumentException() {
+        int[] unconnected = new int[] { 1, 1, 3, 2 };
+        assertThrows(IllegalArgumentException.class, () -> sc3x4.removeVerticalSeam(unconnected));
+    }
+
+    @Test
+    public void removeVerticalSeam_OutOfBoundsSeam_ThrowsIllegalArgumentException() {
+        int[] outOfBounds = new int[] { 5, 6, 6, 7 };
+        assertThrows(IllegalArgumentException.class, () -> sc3x4.removeVerticalSeam(outOfBounds));
+    }
+
+    @Test
+    public void removeVerticalSeam_PictureWithWidth1px_ThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sc1x1.removeVerticalSeam(new int[] { 0 }));
+    }
+
+    @Test
+    public void removeHorizontalSeam_PictureWithHeight1px_ThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sc1x1.removeHorizontalSeam(new int[] { 0 }));
+    }
+
+    @Test
+    public void findHorizontalSeam_sc3x4_Returns121() {
+        assertArrayEquals(new int[] { 1, 2, 1 }, sc3x4.findHorizontalSeam());
+    }
+
+    @Test
+    public void findHorizontalSeam_sc4x6_Returns1210() {
+        assertArrayEquals(new int[] { 1, 2, 1, 0 }, sc4x6.findHorizontalSeam());
+    }
+
+    @Test
+    public void findVerticalSeam_sc3x4_Returns0110() {
+        assertArrayEquals(new int[] { 0, 1, 1, 0 }, sc3x4.findVerticalSeam());
+    }
+
+    @Test
+    public void findVerticalSeam_sc4x6_Returns121121() {
+        assertArrayEquals(new int[] { 1, 2, 1, 1, 2, 1 }, sc4x6.findVerticalSeam());
+    }
+
+    @Test
+    public void findVerticalSeam_sc7x10_Returns2343433221() {
+        assertArrayEquals(new int[] { 2, 3, 4, 3, 4, 3, 3, 2, 2, 1 }, sc7x10.findVerticalSeam());
     }
 }
